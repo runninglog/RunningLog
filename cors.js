44110847f96b6b407936e586fb31f2b6ffff5014
Caio Begotti <caio1982@gmail.com>
2015-01-16 23:41:49 -0200

    fix the cors setup by simply returning 200 when the method is an OPTIONS, otherwise the clients will get crazy when trying to PUT an entry and it will just return 404 all times

--- cors.js
+++ cors.js
@@ -1,5 +1,12 @@
 module.exports = function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  next();
+  res.header("Access-Control-Allow-Origin", "http://localhost:9000");
+  res.header('Access-Control-Allow-Headers', "Origin, Authorization, Accept, Content-Type, Content-Length, X-Requested-With");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, UPDATE, PATCH, HEAD, DELETE, OPTIONS");
+
+  if ('OPTIONS' == req.method) {
+    res.sendStatus(200);
+  }
+  else {
+    next();
+  }
 }
