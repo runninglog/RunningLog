fba7c3a1f87d1b4eef5a53bc00d468e82e2e389b
Caio Begotti <caio1982@gmail.com>
2015-01-17 23:35:16 -0200

    better logging messages while we are developing it

--- config/passport/basic.js
+++ config/passport/basic.js
@@ -1,20 +1,33 @@
+var logger = require('../../utils/logger');
 var basicStrategy = require('passport-http').BasicStrategy;
 
 module.exports = function (app, config) {
     var basic = new basicStrategy(
         function(username, password, callback) {
             app.models.users.findOne({ username: username }, function (err, user) {
-                if (err) { return callback(err); }
+                if (err) {
+                    logger.error('Access denied, general error: ' + err);
+                    return callback(err);
+                }
 
                 // No user found with that username
-                if (!user) { return callback(null, false); }
+                if (!user) {
+                    logger.debug('Access denied, unknown: ' + username);
+                    return callback(null, false);
+                }
 
                 // Make sure the password is correct
                 user.verifyPassword(password, function(err, isMatch) {
-                    if (err) { return callback(err); }
+                    if (err) {
+                        logger.error('Access denied, verification error: ' + err);
+                        return callback(err);
+                    }
 
                     // Password did not match
-                    if (!isMatch) { return callback(null, false); }
+                    if (!isMatch) {
+                        logger.debug('Access denied, credentials error: ' + username);
+                        return callback(null, false);
+                    }
 
                     // Success
                     return callback(null, user);
