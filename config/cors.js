41e6cd86dc982c34e2941828fb387983ad679709
Caio Begotti <caio1982@gmail.com>
2015-01-17 23:34:32 -0200

    syntax OCD

--- config/cors.js
+++ config/cors.js
@@ -1,12 +1,12 @@
 module.exports = function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:9000");
-  res.header('Access-Control-Allow-Headers', "Origin, Authorization, Accept, Content-Type, Content-Length, X-Requested-With");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, UPDATE, PATCH, HEAD, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Origin", "http://localhost:9000");
+    res.header('Access-Control-Allow-Headers', "Origin, Authorization, Accept, Content-Type, Content-Length, X-Requested-With");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, UPDATE, PATCH, HEAD, DELETE, OPTIONS");
 
-  if ('OPTIONS' == req.method) {
-    res.sendStatus(200);
-  }
-  else {
-    next();
-  }
-}
+    if ('OPTIONS' == req.method) {
+        res.sendStatus(200);
+    }
+    else {
+        next();
+    }
+};
