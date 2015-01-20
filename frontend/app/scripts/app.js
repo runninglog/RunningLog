3f7e5e179b3a18e466fe4a5e57700f3922127b9a
Caio Begotti <caio1982@gmail.com>
2015-01-19 23:10:05 -0200

    new race creation view to match the overall changes in look of the views, though the POST is still pending :-P

--- frontend/app/scripts/app.js
+++ frontend/app/scripts/app.js
@@ -9,6 +9,9 @@ angular.module('ngdemoApp', [
   $routeProvider.when('/user-list', {templateUrl: 'views/user-list.html', controller: 'UserListCtrl'});
   $routeProvider.when('/user-detail/:id', {templateUrl: 'views/user-detail.html', controller: 'UserDetailCtrl'});
   $routeProvider.when('/user-creation', {templateUrl: 'views/user-creation.html', controller: 'UserCreationCtrl'});
+  $routeProvider.when('/race-list', {templateUrl: 'views/race-list.html', controller: 'RaceListCtrl'});
+  $routeProvider.when('/race-detail/:id', {templateUrl: 'views/race-detail.html', controller: 'RaceDetailCtrl'});
+  $routeProvider.when('/race-creation', {templateUrl: 'views/race-creation.html', controller: 'RaceCreationCtrl'});
   $routeProvider.otherwise({redirectTo: '/dummy'});
 
   $httpProvider.defaults.useXDomain = true;
