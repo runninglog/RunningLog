9142434a1f413b10ec5adc04316f3d53e9d43e3b
Caio Begotti <caio1982@gmail.com>
2015-02-14 20:58:53 -0200

    change how the databases are named so they can be automatically called using the same NODE_ENV var used elsewhere, thus easing how we run the tests by ensuring the old database is destroyed/created accordingly

--- config/env/production.js
+++ config/env/production.js
@@ -4,7 +4,7 @@ var mongodb_port = process.env.MONGO_PORT_27017_TCP_PORT || '27017';
 var mongodb_addr = process.env.MONGO_PORT_27017_TCP_ADDR || '127.0.0.1';
 
 module.exports = {
-    db: 'mongodb://' + mongodb_addr + ':' + mongodb_port + '/runninglog_prd',
+    db: 'mongodb://' + mongodb_addr + ':' + mongodb_port + '/runninglog_' + process.env.NODE_ENV,
     serverPort: process.env.SERVER_PORT || 8443,
     serverOptions: {
         // Certificates should not be self-signed in production
